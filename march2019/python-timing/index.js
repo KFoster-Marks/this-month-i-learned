15844e47375536d752d281bf3da6d823593e00dc
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-03-26 18:32:52 -0600

    Add file with sleep() example

---
+++ march2019/python-timing/index.js
@@ -0,0 +1,10 @@
+from time import time, sleep
+
+seconds = int(input('How many seconds would you like to wait? '))
+
+start_time = time()
+sleep(seconds)
+end_time = time()
+total_time = end_time - start_time
+
+print('This program took {} seconds to run'.format(total_time))
