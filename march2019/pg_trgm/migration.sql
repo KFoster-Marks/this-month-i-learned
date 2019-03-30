ec9bc83f93f8052a59fc3c4cec3d164422859f82
Kristen Foster-Marks <kristen.foster-marks@zayo.com>
2019-03-30 07:22:28 -0600

    Create migration script for simple account name full text search

--- march2019/pg_trgm/migration.sql
+++ march2019/pg_trgm/migration.sql
@@ -1,9 +1,14 @@
-DROP materialized view IF EXISTS accounts;
-CREATE materialized view accounts AS
-    (
-        SELECT row_number() OVER(order by account_name), account_name
-        FROM ts_stat('select name from org.accounts')
-    );
+DROP materialized view IF EXISTS search_account;
+CREATE materialized view search_account AS (
+    SELECT 
+        COALESCE(acct.name, ''::character varying) AS name,
+        COALESCE(acct.type, ''::character varying) AS type,
+        (COALESCE(acct.name, ''::character varying)::text) AS search_fields,
+        (setweight(to_tsvector(COALESCE(acct.name, ''::character varying)::text), 'A'::"char")) AS search_document
+    FROM org.accounts acct
+    WHERE acct.type::text = ANY (ARRAY['Primary'::character varying::text, 'Billing'::character varying::text])
+    ORDER BY (COALESCE(acct.name, ''::character varying));
+);
 
 DROP index IF EXISTS org.accounts_idx_name;
-CREATE index accounts_idx_name ON org.accounts USING gin(account_name extensions.gin_trgm_ops);
+CREATE index search_account_idx_name ON org.accounts USING gin(account_name extensions.gin_trgm_ops);
